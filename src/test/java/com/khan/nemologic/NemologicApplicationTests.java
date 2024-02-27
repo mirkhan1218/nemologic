649535a919f7ffca80e6ad75ee6edaa106c77277
mirkhan <mirkhan1218@gmail.com>
2024-02-27 11:48:55 +0900

    프로젝트 생성

---
+++ src/test/java/com/khan/nemologic/NemologicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.nemologic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemologicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
